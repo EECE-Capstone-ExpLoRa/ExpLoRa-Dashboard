70c7d5b338d95d2f1b6ee18650c1351fe341782d
Michael Briggs <michaelbriggs29@yahoo.com>
2023-01-29 23:25:50 -0500

    Wrote simple query

--- backend/src/timestream/timestream.controller.ts
+++ backend/src/timestream/timestream.controller.ts
@@ -4,6 +4,11 @@ import { TimestreamService } from "./timestream.service";
 @Controller('timestream')
 export class TimestreamController {
     constructor(private readonly timestreamService: TimestreamService) {}
+    
+    @Get('db/list')
+    listAllDatabases() {
+        return this.timestreamService.listDatabases();
+    }
 
     @Post('db/:dbName')
     createDb(@Param('dbName') dbName: string) {
@@ -18,5 +23,15 @@ export class TimestreamController {
     @Get('db/:dbName')
     getTableDescription(@Param('dbName') dbName: string, @Body('tableName') tableName: string) {
         return this.timestreamService.getTableDescription(dbName, tableName);
+    }
+
+    @Get('db/:dbName/list')
+    listAllTables(@Param('dbName') dbName: string) {
+        return this.timestreamService.getTablesList(dbName);
+    }
+
+    @Get(':db/:tableName')
+    getTableData(@Param('db') db: string, @Param('tableName') tableName: string) {
+        return this.timestreamService.getTableData(db, tableName);
     }
 }
--- backend/src/timestream/timestream.service.ts
+++ backend/src/timestream/timestream.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@nestjs/common";
-import { TimestreamQueryClient } from "@aws-sdk/client-timestream-query";
-import { TimestreamWriteClient, CreateDatabaseCommand, DeleteDatabaseCommand, DescribeTableCommand } from "@aws-sdk/client-timestream-write";
+import { TimestreamQueryClient, QueryCommand, QueryCommandOutput, QueryCommandInput } from "@aws-sdk/client-timestream-query";
+import { TimestreamWriteClient, CreateDatabaseCommand, DeleteDatabaseCommand, ListDatabasesCommand, DescribeTableCommand, ListTablesCommand } from "@aws-sdk/client-timestream-write";
 
 
 @Injectable()
 export class TimestreamService {
-    private queryClient = new TimestreamQueryClient({region: "us-east-1"});
-    private writeClient = new TimestreamWriteClient({region: "us-east-1" });
+    private queryClient: TimestreamQueryClient = new TimestreamQueryClient({region: "us-east-1"});
+    private writeClient: TimestreamWriteClient = new TimestreamWriteClient({region: "us-east-1" });
 
     async createDatabase(dbName: string) {
         const db = {
@@ -49,6 +49,32 @@ export class TimestreamService {
         }
     };
 
+    listDatabases() { //I dont understand this or the helper function lol
+        const listDatabaseParams = {
+            MaxResults: 20
+        };
+        const command = new ListDatabasesCommand(listDatabaseParams);
+        this.listDatabasesHelper(listDatabaseParams, null, command);
+    };
+
+    private async listDatabasesHelper(params, nextToken: string, command: ListDatabasesCommand) {
+        if (nextToken) {
+            params.nextToken = nextToken;
+        }
+
+        try {
+            const data = await this.writeClient.send(command);
+            data.Databases.forEach((db) => console.log(db.DatabaseName));
+
+            if (data.NextToken) {
+                return this.listDatabasesHelper(params, data.NextToken, command);
+            }
+        }
+        catch(error) {
+            console.log("Error while listing databases", error);
+        }
+    }
+
     async getTableDescription(dbName: string, tableName: string) {
         const descriptionParams = {
             DatabaseName: dbName,
@@ -71,6 +97,54 @@ export class TimestreamService {
         }
         catch(error) {
             throw error;
+        }
+    };
+
+    async getTableData(db: string, tableName: string, nextToken: string = null) {
+        const queryRequest: string = `SELECT * FROM ${db}.${tableName}`;
+        const queryRequestParams: QueryCommandInput = {
+            QueryString: queryRequest
+        };
+
+        const queryCommand: QueryCommand = new QueryCommand(queryRequestParams);
+        try {
+            const response: QueryCommandOutput = await this.queryClient.send(queryCommand);
+            console.log(`Whole response: ${JSON.stringify(response)}`);
+            console.log(response.Rows);
+        }
+        catch(error) {
+            console.log(`An error occured when trying to run the query: ${queryRequest}\nError Trace: ${error}`);
+        }
+        // if (nextToken) {
+        //     queryRequestParams.nextToken = nextToken
+        // }
+        // await this.queryClient.send 
+    }
+
+    getTablesList(dbName: string) { //I dont understand this or the helper function lol
+        const params = {
+            DatabaseName: dbName,
+            MaxResults: 20
+        };
+        const command = new ListTablesCommand(params);
+        this.getTablesListHelper(params, null, command);
+    };
+
+    private async getTablesListHelper(params, nextToken: string, command: ListTablesCommand) {
+        if (nextToken) {
+            params.nextToken = nextToken;
+        }
+
+        try {
+            const data = await this.writeClient.send(command);
+            data.Tables.forEach((table) => console.log(table.TableName));
+
+            if (data.NextToken) {
+                return this.getTablesListHelper(params, data.NextToken, command);
+            }
+        }
+        catch(error) {
+            console.log("Error while listing tables", error);
         }
     }
 };
