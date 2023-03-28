727ebb5fa105ff0e3a95903042af21419944bbae
Jia <jiamu0614@gmail.com>
2023-03-28 01:16:59 -0400

    add datetime range picker to chart

--- frontend/src/components/charts/AccelerationChart.tsx
+++ frontend/src/components/charts/AccelerationChart.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, HStack, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Select, Text, useDisclosure } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Select, Text, useDisclosure } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import {
   XAxis,
@@ -12,52 +12,21 @@ import {
 } from 'recharts';
 import moment from 'moment'
 import { getAcceleration, getAccelerationX, getAccelerationY, getAccelerationZ} from '../../services/timestream.service';
+// @ts-ignore
+import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker'
+import { TimeIcon } from '@chakra-ui/icons'
 
-const chartData = [
-  { timestamp: 1578930642, value: 14 },
-  { timestamp: 1578930648, value: 14 },
-  { timestamp: 1578930655, value: 13 },
-  { timestamp: 1578930662, value: 5 },
-  { timestamp: 1578930668, value: 12 },
-  { timestamp: 1578930674, value: 16 },
-  { timestamp: 1578930681, value: 15 },
-  { timestamp: 1578930687, value: 12 },
-  { timestamp: 1578930694, value: 15 },
-  { timestamp: 1578930701, value: 16 },
-  { timestamp: 1578930707, value: 17 },
-  { timestamp: 1578930713, value: 11 },
-  { timestamp: 1578930720, value: 9 },
-  { timestamp: 1578930727, value: 20 },
-  { timestamp: 1578930734, value: 19 },
-  { timestamp: 1578930741, value: 18 },
-  { timestamp: 1578930747, value: 4 },
-  { timestamp: 1578930753, value: 12 },
-  { timestamp: 1578930760, value: 16 },
-  { timestamp: 1578930766, value: 13 },
-  { timestamp: 1578930773, value: 17 },
-  { timestamp: 1578930780, value: 15 },
-  { timestamp: 1578930786, value: 12 },
-  { timestamp: 1578930792, value: 16 },
-  { timestamp: 1578930799, value: 8 },
-  { timestamp: 1578930805, value: 13 },
-  { timestamp: 1578930812, value: 19 },
-  { timestamp: 1578930819, value: 11 },
-  { timestamp: 1578930825, value: 16 },
-  { timestamp: 1578930831, value: 32 },
-  { timestamp: 1678924389, value: -24 },
-  { timestamp: 1678924395, value: -32 },
-  { timestamp: 1678924401, value: -18 }
-]
-
-const AccelerationCard = ({isOpen, modalSize="full"}: any) => {
+const AccelerationCard = ({modalSize="full"}: any) => {
   const [open, setOpen] = useState(true)
   const [accelerationDir, setAccelerationDir] = useState('x')
+  const now = new Date()
+  const [value, onChange] = useState([new Date(now.getFullYear(), now.getMonth(), now.getDate() - 14), now])
   const { isOpen: isModalOpen, onOpen: onModalOpen, onClose: onModalClose} = useDisclosure() 
 
   const expandableContent = () => {
     return (
       <Box width="full" height="250px" padding={4} backgroundColor="white" shadow={"md"} borderTop={2}>
-        <AccelerationChart accelerationDir={accelerationDir} />
+        <AccelerationChart accelerationDir={accelerationDir} timeRange={value} />
       </Box>
     );
   }
@@ -70,6 +39,22 @@ const AccelerationCard = ({isOpen, modalSize="full"}: any) => {
         <option value='z'>Z</option>
         <option value="all">All</option>
       </Select>
+    );
+  }
+
+  const renderDateTimeRangeSelector = () => {
+    return (
+      <Popover placement='bottom-start' closeOnBlur={false}>
+        <PopoverTrigger>
+          <TimeIcon />
+        </PopoverTrigger>
+        <PopoverContent width="500px">
+        <PopoverCloseButton />
+        <PopoverBody>
+          <DateTimeRangePicker onChange={onChange} value={value} />
+        </PopoverBody>
+        </PopoverContent>
+      </Popover>
     );
   }
 
@@ -86,7 +71,7 @@ const AccelerationCard = ({isOpen, modalSize="full"}: any) => {
 
   const renderModalOpenButton = () => {
     return (
-      <Image onClick={onModalOpen} width={5} height={5} src="/popout.png" />
+      <Image onClick={onModalOpen} marginLeft={5} width={5} height={5} src="/popout.png" />
     );
   }
 
@@ -100,6 +85,7 @@ const AccelerationCard = ({isOpen, modalSize="full"}: any) => {
           </HStack>
           <HStack>
             {renderDirectionSelect("xs")}
+            {renderDateTimeRangeSelector()}
             {renderModalOpenButton()}
           </HStack>
         </Flex>
@@ -112,7 +98,7 @@ const AccelerationCard = ({isOpen, modalSize="full"}: any) => {
           <ModalCloseButton />
           <ModalBody>
             <Box height="600px">
-              <AccelerationChart accelerationDir={accelerationDir} />
+              <AccelerationChart accelerationDir={accelerationDir} timeRange={value} />
             </Box>
             {renderDirectionSelect("sm")}
           </ModalBody>
@@ -127,30 +113,30 @@ const AccelerationCard = ({isOpen, modalSize="full"}: any) => {
   );
 }
 
-const minTimeQueryParam = 1672550827000;
-const maxTimeQueryParam = 1679891227000;
 
-export const AccelerationChart = ({accelerationDir}: {accelerationDir: string}) => {
+export const AccelerationChart = ({accelerationDir, timeRange}: {accelerationDir: string, timeRange: Date[]}) => {
   const [accelerationXData, setAccelerationXData] = useState([])
   const [accelerationYData, setAccelerationYData] = useState([])
   const [accelerationZData, setAccelerationZData] = useState([])
   const [accelerationData, setAccelerationData] = useState([])
 
+  const [minTimeQueryParam, maxTimeQueryParam] = timeRange
+
   useEffect(() => {
-    getAccelerationX("00-80-00-00-04-05-b6-b1", minTimeQueryParam, maxTimeQueryParam).then((res) => {
+    getAccelerationX("00-80-00-00-04-05-b6-b1", minTimeQueryParam.getTime(), maxTimeQueryParam.getTime()).then((res) => {
       setAccelerationXData(res)
     })
-    getAccelerationY("00-80-00-00-04-05-b6-b1", minTimeQueryParam, maxTimeQueryParam).then((res) => {      
+    getAccelerationY("00-80-00-00-04-05-b6-b1", minTimeQueryParam.getTime(), maxTimeQueryParam.getTime()).then((res) => {      
       setAccelerationYData(res)
     })
-    getAccelerationZ("00-80-00-00-04-05-b6-b1", minTimeQueryParam, maxTimeQueryParam).then((res) => {      
+    getAccelerationZ("00-80-00-00-04-05-b6-b1", minTimeQueryParam.getTime(), maxTimeQueryParam.getTime()).then((res) => {      
       setAccelerationZData(res)
     })
-    getAcceleration("00-80-00-00-04-05-b6-b1", minTimeQueryParam, maxTimeQueryParam).then((res) => {
+    getAcceleration("00-80-00-00-04-05-b6-b1", minTimeQueryParam.getTime(), maxTimeQueryParam.getTime()).then((res) => {
       setAccelerationData(res)
     })
 
-  }, [])  
+  }, [minTimeQueryParam, maxTimeQueryParam])  
 
   const getAccelerationData = () => {
     switch(accelerationDir) {
