277070ac599331cede2d8bd5b193abdf8cc3ab53
Peter Rydzynski <peter.rydzynski@gmail.com>
2023-04-10 22:45:01 -0400

    Update landing page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,7 +1,24 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 import React from "react";
 // importing Link from react-router-dom to navigate to
 // different end points.
+
+// const Home = () => {
+//   return (
+//     <Flex
+//       direction="column"
+//       align="center"
+//       justify="flex-start"
+//       backgroundColor="gray.100"
+//       h="100vh"
+//       textColor="brand.500"
+//     >
+//       <Flex margin='14px 0px' padding='0px 8px'>
+//         <Image src="/Explora-Name.svg" height='16' />
+//       </Flex>
+//     </Flex>
+//   );
+// };
 
 const Home = () => {
   return (
@@ -13,50 +30,19 @@ const Home = () => {
       h="100vh"
       textColor="brand.500"
     >
-      <Box marginTop="12px" fontSize="2xl" padding="8px">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Viverra mauris in
-        aliquam sem fringilla. Purus in massa tempor nec. Magna fringilla urna
-        porttitor rhoncus. Viverra nam libero justo laoreet sit amet. Turpis
-        massa tincidunt dui ut ornare lectus sit amet est. Duis tristique
-        sollicitudin nibh sit amet commodo nulla. Ac turpis egestas integer eget
-        aliquet nibh. Risus sed vulputate odio ut enim blandit volutpat. Non
-        diam phasellus vestibulum lorem sed risus ultricies. Hendrerit gravida
-        rutrum quisque non tellus orci. Sodales ut eu sem integer vitae justo.
+      <Flex margin='14px 0px' padding='0px 8px'>
+        <Image src="/Explora-Name.svg" height='48' />
+      </Flex>
+      <Box maxWidth="800px" fontSize="xl" padding="8px" textAlign="center">
+        ExpLoRa is a telemetry system based on LoRaWAN technology designed for high speed objects such as model rockets and race cars.
       </Box>
-      <Box marginTop="12px" fontSize="2xl" padding="8px">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Viverra mauris in
-        aliquam sem fringilla. Purus in massa tempor nec. Magna fringilla urna
-        porttitor rhoncus. Viverra nam libero justo laoreet sit amet. Turpis
-        massa tincidunt dui ut ornare lectus sit amet est. Duis tristique
-        sollicitudin nibh sit amet commodo nulla. Ac turpis egestas integer eget
-        aliquet nibh. Risus sed vulputate odio ut enim blandit volutpat. Non
-        diam phasellus vestibulum lorem sed risus ultricies. Hendrerit gravida
-        rutrum quisque non tellus orci. Sodales ut eu sem integer vitae justo.
+      <Box maxWidth="800px" fontSize="xl" padding="8px" textAlign="center">
+        With ExpLoRa, you can easily track the location, altitude, acceleration, and more of your vehicles in real time. Our system is easy to use, affordable, and reliable, making it the perfect solution for hobbyists and professionals alike. Make an account and add your devices to get started!
       </Box>
-      <Box marginTop="12px" fontSize="2xl" padding="8px">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Viverra mauris in
-        aliquam sem fringilla. Purus in massa tempor nec. Magna fringilla urna
-        porttitor rhoncus. Viverra nam libero justo laoreet sit amet. Turpis
-        massa tincidunt dui ut ornare lectus sit amet est. Duis tristique
-        sollicitudin nibh sit amet commodo nulla. Ac turpis egestas integer eget
-        aliquet nibh. Risus sed vulputate odio ut enim blandit volutpat. Non
-        diam phasellus vestibulum lorem sed risus ultricies. Hendrerit gravida
-        rutrum quisque non tellus orci. Sodales ut eu sem integer vitae justo.
-      </Box>
-      <Box marginTop="12px" fontSize="2xl" padding="8px">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Viverra mauris in
-        aliquam sem fringilla. Purus in massa tempor nec. Magna fringilla urna
-        porttitor rhoncus. Viverra nam libero justo laoreet sit amet. Turpis
-        massa tincidunt dui ut ornare lectus sit amet est. Duis tristique
-        sollicitudin nibh sit amet commodo nulla. Ac turpis egestas integer eget
-        aliquet nibh. Risus sed vulputate odio ut enim blandit volutpat. Non
-        diam phasellus vestibulum lorem sed risus ultricies. Hendrerit gravida
-        rutrum quisque non tellus orci. Sodales ut eu sem integer vitae justo.
-      </Box>
+      <Flex margin='14px 0px' padding='0px 8px'>
+        <Image src="/homepage_rocket.svg" height='48' />
+      </Flex>
+      
     </Flex>
   );
 };
