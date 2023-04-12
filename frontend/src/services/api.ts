e3e23597dcb058fd2a16cc68bb2cab0ebb90e42a
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-04-12 01:55:33 -0400

    so close to giving up and never looking at this again

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,5 +4,6 @@ const exploraApi = axios.create({
     baseURL: process.env.SERVER || 'http://localhost:8080',
 });
 
+console.log(`Server URL: ${process.env.SERVER}`);
 console.log(exploraApi.defaults.baseURL);
 export default exploraApi;
