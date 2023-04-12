f7f56fadf07fcde4acc9273d592caa30fc47d96b
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-04-12 01:06:23 -0400

    added backend url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const exploraApi = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.SERVER_URL || 'http://localhost:8080',
 });
 
 export default exploraApi;
