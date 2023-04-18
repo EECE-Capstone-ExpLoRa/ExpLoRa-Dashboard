dcb12f8bf90859f3747f15dc77ed3d5f143af17e
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-04-18 10:55:25 -0400

    pick this up vercel

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,6 +4,6 @@ const exploraApi = axios.create({
     baseURL: process.env.REACT_APP_SERVER || 'http://localhost:8080',
 });
 
-console.log(`Server URL: ${process.env.REACT_APP_SERVER}`);
+console.log(`Server URL: ${process.env.REACT_APP_SERVER} pushes arent logging`);
 console.log(exploraApi.defaults.baseURL);
 export default exploraApi;
